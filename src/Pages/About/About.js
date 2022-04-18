b8d87292c52ba803d1d0fe800e25568ca1f456b4
Taposhpaul <taposhp05@gmail.com>
2022-04-18 18:51:48 +0600

    added about and my goal

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className='w-50 mx-auto border rounded m-4 p-3'>
             <h4 className='text-center'>Name: Taposh paul</h4>
-            <p> </p>
+            <p>My Goal: My goal is best wab devolapment. There are many goals that a web developer may have. But My ones include creating visually appealing and user-friendly websites. Developing sites that are optimized for search engine ranking, and ensuring that websites are properly coded and functional.</p>
         </div>
     );
 };
